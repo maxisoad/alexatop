5ec5e1c1f9be09a160c0216eb8794a0983e11a2e
Maxi <maxisoad@hotmail.com>
2014-06-10 17:01:44 -0300

    first version

---
+++ alexatop.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+#requires httrack
+
+import urllib.request
+import re
+import os
+import sys
+
+def retrieveTop(quantity):
+    wfile = open("500.txt","w")
+    page = "http://www.alexa.com/topsites/global;"
+    pattern = r"<a href=\"/siteinfo/(.*?)\">(.*?)</a>"
+    #command = "wget -q -l 1 -r -k -P webpages "
+    command = 'cd webpages && mkdir <DIR> && cd <DIR> && wget -E -H -k -K -p <URL>'
+    #command = "httrack <URL> -nr2N101 -O webpages/<DIR>"# -P proxy.unlu.edu.ar:8080"
+    position = 1
+
+    os.system('rm webpages -R')
+    os.system('mkdir webpages')
+
+    for i in range(0, 1):#20):
+        actualurl = page+str(i)
+        content = urllib.request.urlopen(actualurl).read()
+        match = re.findall(pattern, content.decode())
+        for webpage in match:
+            print(str(position)+' - '+webpage[1])
+            wfile.write(webpage[1]+'\r\n')
+            connection = urllib.request.urlopen('http://www.'+webpage[1])
+            #print(str(connection.geturl()))
+            execution = command.replace('<URL>',connection.geturl()).replace('<DIR>',webpage[1])
+            print(execution)
+            os.system(execution)
+            position += 1
+            if position > quantity:
+                break
+
+if __name__ == "__main__":
+    try:
+        quantity = int(sys.argv[1])
+    except Exception as e:
+        sys.exit('Usage: top500.py quantity(number)')
+    retrieveTop(quantity)
